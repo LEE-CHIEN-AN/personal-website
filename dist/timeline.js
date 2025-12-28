9eae93d5483c596a5f76957b237d702711679046
li_jieeeee <b12705041@ntu.edu.tw>
2025-12-28 22:01:36 +0800

    fix timeline date display

--- dist/timeline.js
+++ dist/timeline.js
@@ -129,7 +129,8 @@ function createTimelineEventHTML(event) {
       </div>
       <div class="timeline-description">${escapeHtml(event.description)}${linkHTML}</div>
       <div class="event-date">
-        <div>${day || ''}</div>
+        <div class="day">${day || ''}</div>
+        <div>${mon || ''}</div>
       </div>
       <div class="event-category">${escapeHtml(event.category)}</div>
     </div>
